ce317ce122e63b891ee1b57fbd5155472fa1b74c
Ömer Taşkesen <58364159+omertaske@users.noreply.github.com>
2025-06-23 23:30:36 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,5 +3,5 @@ const outputEl = document.querySelector('#name-output');
 
 inputEl.addEventListener("input" , ()=> {
     const value = inputEl.value.trim();
-    outputEl.textContent = value ==="" ? "anonymous" : value ;
+    outputEl.textContent = value ==="" ? "Anonymous" : value ;
 });
